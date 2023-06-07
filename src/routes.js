1e4928183be2e8b2ddd1e2c39b9f356c359a239c
EricksonLuiiz <erickson.delfino@sisplansistemas.com.br>
2023-06-07 13:34:49 -0300

    Ajustado rota de retorno dos id

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express"); // importa o express
 
+const { Router } = require("express"); // importa o express
+const ContactController = require("./app/controllers/ContactController"); // importa o controller
 const router = Router(); // define a variável com o valor do express
 
-const ContactController = require("./app/controllers/ContactController"); // importa o controller
 
 router.get("/contacts", ContactController.index); // passa o get pela rota e usa o index de dentro do controller.
-router.get("/contacts/id:", ContactController.show);
+router.get("/contacts/:id", ContactController.show);
 module.exports = router;
